cc717e9d4f41fdcf3a61df4f1c2bcedda6d59af5
Mathias Bosman <bosmanmathias@gmail.com>
2015-07-05 22:23:36 +0200

    jQuery added

--- app/views/header.php
+++ app/views/header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang='<?php echo DEFAULT_LANGUAGE; ?>'>
     <head>
         <title><?php echo SITE_NAME; ?></title>
+        <meta charset='<?php echo CHARSET; ?>'>
+
+        <?php
+        // Main javascript files
+        $html->includeJs('jQuery/jquery-2.1.4.min');
+        ?>
     </head>
     
     <body>
