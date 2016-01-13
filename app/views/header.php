bb4c70764509f29a1e05e1678476a36b6dd48ab2
Mathias Bosman <bosmanmathias@gmail.com>
2016-01-13 22:02:24 +0100

    Base UI
    Started with some simple css

--- app/views/header.php
+++ app/views/header.php
@@ -12,3 +12,22 @@
     </head>
 
     <body>
+<header id="site-header">
+    <nav class="grid" id="main-nav">
+        <div class="row">
+            <ul class="column-5">
+                <li class="column-4"><a href="home">Home</a></li>
+                <li class="column-4"><a href="">Page 2</a></li>
+                <li class="column-4"><a href="">Page 3</a></li>
+            </ul>
+            <div class="column-2 logo">
+                <img src="assets/img/logo-small.png" />
+            </div>
+            <ul class="column-5">
+                <li class="column-4"><a href="">Page 4</a></li>
+                <li class="column-4"><a href="">Page 5</a></li>
+                <li class="column-4"><a href="login">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ app/views/user/login.php
@@ -0,0 +1,14 @@
+<header class="title">
+    <h1>Login</h1>
+</header>
+
+<section class="grid" id="login">
+    <div class="row">
+        <form class="column-9">
+            test
+        </form>
+        <aside class="column-3">
+            test
+        </aside>
+    </div>
+</section>
