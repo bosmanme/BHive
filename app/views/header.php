498fc18d44d8c2d25e5d3c84cbe16552530407ea
Mathias Bosman <bosmanmathias@gmail.com>
2016-01-21 22:02:42 +0100

    Google's material symbols
    Material font added.
    Info on https://design.google.com/icons/

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,8 @@
 
         <!-- Google font Robot -->
         <link href="//fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en" rel="stylesheet" type="text/css">
+        <!-- Google's material symbols font @see https://design.google.com/icons/ -->
+        <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <link rel="stylesheet" href="assets/css/material.css" />
     </head>
