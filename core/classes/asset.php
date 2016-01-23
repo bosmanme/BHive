6aa3f818a537b1af7f1f9a340f43d75835f5c53b
mathiasbosman <bosmanmathias@gmail.com>
2016-01-23 01:14:57 +0100

    Further work on the Asset class

--- core/classes/asset.php
+++ core/classes/asset.php
@@ -25,18 +25,24 @@ class Asset
 	 *
 	 * @var  array
 	 */
-    protected static $_instances = [];
+    protected static $_instances = [
+        'css' => [
+            // 'main' => 'assets/css/main.css',
+        ],
+    ];
 
     /**
 	 * Default configuration values
 	 *
 	 * @var  array
 	 */
-	protected static $_config = array(
+	protected static $_config = [
 		'paths'   => ['assets/'],
 		'img_dir' => 'img/',
 		'js_dir'  => 'js/',
 		'css_dir' => 'css/',
+
+        // Custom folders for each type
 		'folders' => [
 			'css' => [],
 			'js'  => [],
@@ -48,7 +54,7 @@ class Asset
 		'indent_with'     => "\t",
 		'auto_render'     => true,
 		'fail_silently'   => false,
-	);
+	];
 
     /**
 	 * This is called automatically by the Autoloader.  It loads in the config
@@ -62,9 +68,33 @@ class Asset
 
 	}
 
-    protected static function _load($type, $file)
+    /**
+     * Try to return the path for a certain file
+     *
+     * @param   string  $type   The type of the file (css, img, js,...)
+     * @param   string  $file   The name of the file
+     * @return  string
+     */
+    protected static function _getPath($file, $type)
     {
+        $searchFolders = [];
 
+        // Top level config set?
+        $key = $type . '_dir';
+        if (array_key_exists($key, static::$_config)) {
+            $searchFolders[] = static::$_config[$key];
+        }
+
+        // Custom folder set?
+        if (array_key_exists('folders', static::$_config)) {
+            if (array_key_exists($type, static::$_config['folders'])) {
+                $searchFolders = array_merge($searchFolders, static::$_config['folders'][$type]);
+            }
+        }
+
+        if ( ! empty($searchFolders)) {
+            //TODO finish
+        }
     }
 
     public static function css($file)
